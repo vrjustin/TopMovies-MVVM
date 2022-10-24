a9b34de1f5cbdfef4149f87aa19eeb3ec0d5c7ae
Justin Maronde <myemailisjustin@me.com>
2022-10-24 07:48:05 -0400

    * Removed templated storyboard and all references to it.
    * Add in Starting ViewController launched from the SceneDelegate to TopMoviesViewController.

---
+++ TopMovies/ViewControllers/TopMoviesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoviesViewController.swift
+//  TopMovies
+//
+//  Created by Justin Maronde on 10/24/22.
+//
+
+import Foundation
+import UIKit
+
+class TopMoviesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .red
+    }
+    
+    
+}
