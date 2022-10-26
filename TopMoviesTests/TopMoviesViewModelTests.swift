287af51bb62d26041c9019be21b9f42823c118cd
Justin Maronde <myemailisjustin@me.com>
2022-10-26 07:36:26 -0400

    * removed original schemes from starting app. added in new schemes for production and dev * add testing target for app * placeholder of tests for topmoviesviewmodel

---
+++ TopMoviesTests/TopMoviesViewModelTests.swift
@@ -0,0 +1,20 @@
+//
+//  TopMoviesViewModelTests.swift
+//  TopMoviesTests
+//
+//  Created by Justin Maronde on 10/26/22.
+//
+
+import XCTest
+
+final class TopMoviesViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
